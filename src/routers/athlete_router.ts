1799496768fb9538a3198eed09bf360c864dc77d
leonardoporcelli <104975119+leonardoporcelli@users.noreply.github.com>
2022-05-18 01:32:26 +0200

    Update athlete_router.ts

--- src/routers/athlete_router.ts
+++ src/routers/athlete_router.ts
@@ -1,7 +1,34 @@
-import express = require('express');
-/** apis for athletes */
-export const athlete_router = express.Router();
+const express = require('express');
+const athlete_router = express.Router();
+const Athlets = require('/models/ath')
 
-athlete_router.get('/test', async (req, res) => {
-  res.json({ success: 1 });
-});
+// Getting all
+athlete_router.get('/', async (req, res) => {
+    try{
+        const athlets = await Athlets.find();
+        res.json(athlets);
+    }catch (err){
+        res.status(500).json({ message: err.message })
+    }
+})
+
+// Creating One
+athlete_router.post('/', async (req, res) => {
+    const athlete = new Athlets({
+        id: req.body.id,
+        name: req.body.name,
+        surname: req.body.surname,
+        club: req.body.club,
+        gender: req.body.gender,
+        weight: req.body.weight,
+        birth_year: req.body.birth_year
+    })
+    try{
+        const newAthlete = await athlete.save();
+        res.status(201).json(newAthlete)
+    }catch(err){
+        res.status(400).json({ message: err.message });
+    }
+})
+
+module.exports = athlete_router;
