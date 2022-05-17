97fe21a974b1ca28b9094226b6012dab95972cb5
Marrocco-Simone <marrocco.simone00@gmail.com>
2022-05-17 14:48:07 +0200

    added athlete schema and types

---
+++ src/schemas/Athlete.ts
@@ -0,0 +1,33 @@
+import { model, Schema, SchemaTypes } from "mongoose";
+
+interface AthleteInterface  {
+    _id: String,
+    name: String,
+    surname: String,
+    competition_id?: String,
+    club: String,
+    gender: "M"|"F",
+    weight: Number,
+    birth_year: Number,
+    category_id?: String,
+}
+
+const AthleteSchema = new Schema<AthleteInterface>({
+    _id: SchemaTypes.ObjectId,
+    name: String,
+    surname: String,
+    competition_id: {
+        type: SchemaTypes.ObjectId,
+        ref: "Competition",
+    },
+    club: String,
+    gender: String,
+    weight: Number,
+    birth_year: Number,
+    category_id: {
+        type: SchemaTypes.ObjectId,
+        ref: "Category",
+    },
+})
+
+export const Athlete = model("Athlete", AthleteSchema);
