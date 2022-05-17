fab2857a0c704a254c9d2ad926a4bba72dff61c8
Marrocco-Simone <marrocco.simone00@gmail.com>
2022-05-17 14:49:21 +0200

    added category schema and types

---
+++ src/schemas/Category.ts
@@ -0,0 +1,20 @@
+import { model, Schema, SchemaTypes } from "mongoose";
+
+interface CategoryInterface  {
+    _id: String,
+    age_class_id?: String,
+    max_weight: String,
+    gender: "M"|"F",
+}
+
+const CategorySchema = new Schema<CategoryInterface>({
+    _id: SchemaTypes.ObjectId,
+    age_class_id: {
+        type: SchemaTypes.ObjectId,
+        ref: "AgeClass",
+    },
+    max_weight: String,
+    gender: String,
+})
+
+export const Category = model("Category", CategorySchema);
