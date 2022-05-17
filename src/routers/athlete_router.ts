76227d0f7d7a48a705832ed71342d56216ebe486
leonardoporcelli <104975119+leonardoporcelli@users.noreply.github.com>
2022-05-18 01:38:46 +0200

    Update athlete_router.ts

--- src/routers/athlete_router.ts
+++ src/routers/athlete_router.ts
@@ -1,4 +1,12 @@
+// export {}; //needed or typescripts gives some strange errors
 const express = require('express');
+// /** apis for athletes */
+// export const athlete_router = express.Router();
+
+// athlete_router.get('/test', async (req,res) => {
+//    res.json({success: 1});
+// });
+
 const athlete_router = express.Router();
 const Athlets = require('/models/ath')
 
