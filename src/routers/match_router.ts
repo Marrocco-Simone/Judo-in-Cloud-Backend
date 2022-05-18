5b842f88dd3ef44224a212d06cda22be4f121384
Marrocco-Simone <marrocco.simone00@gmail.com>
2022-05-18 16:57:54 +0200

    added post match route

--- src/routers/match_router.ts
+++ src/routers/match_router.ts
@@ -56,3 +56,33 @@ match_router.get('/:matchid', async (req, res) => {
     error(res, e.message);
   }
 });
+
+match_router.post('/:matchid', async (req, res) => {
+  try {
+    const match_id = req.params.matchid;
+    const match = await Match.findById(match_id);
+    if (match == null) return fail(res, 'Match not found', 404);
+    const body: {
+      winner_athlete: string,
+      is_started: boolean;
+      is_over: boolean;
+      match_scores: {
+        final_time: number;
+        white_ippon: number;
+        white_wazaari: number;
+        white_penalties: number;
+        red_ippon: number;
+        red_wazaari: number;
+        red_penalties: number;
+      };
+    } = req.body;
+    if (body.winner_athlete) match.winner_athlete = body.winner_athlete;
+    if (body.is_started) match.is_started = body.is_started;
+    if (body.is_over) match.is_over = body.is_over;
+    if (body.match_scores) match.match_scores = body.match_scores;
+    await match.save();
+    success(res, match);
+  } catch (e) {
+    error(res, e.message);
+  }
+});
