16fc6de8b283f3979aea926f4a488e62c337c570
Marrocco-Simone <marrocco.simone00@gmail.com>
2022-05-18 23:08:46 +0200

    pulizia

--- src/routers/match_router.ts
+++ src/routers/match_router.ts
@@ -86,13 +86,3 @@ match_router.post('/:matchid', async (req, res) => {
     error(res, e.message);
   }
 });
-
-// Getting all
-match_router.get('/:matchid', async (req, res) => {
-    try{
-        const match = await Match.find();
-        res.json(match);
-    }catch (err){
-        res.status(500).json({ message: err.message })
-    }
-})
