c89ad3215e223d924c2c3ae13aafe9efbe95fcd7
Marrocco-Simone <marrocco.simone00@gmail.com>
2022-05-18 15:25:59 +0200

    working on match routes

--- src/routers/match_router.ts
+++ src/routers/match_router.ts
@@ -1,7 +1,32 @@
 import express = require('express');
+import { match } from '../schemas/Match';
+/* ELIMINA */import { athlete } from '../schemas/Athlete'
 /** api for matches */
 export const match_router = express.Router();
 
-match_router.get('/test', async (req, res) => {
-  res.json({ success: 1 });
+/* ELIMINA */match_router.get('/inizialize', async (req, res) => {
+  try{
+    athlete.insertMany([{
+      name: 'Mario',
+      surname: 'Rossi',
+      club: 'Judo Kodokan Lavis',
+      gender: 'M',
+      weight: 66,
+      birth_year: 2000,
+    }, {
+      name: 'Giuseppe Giovanni',
+      surname: 'Esposito',
+      club: 'Judo e Movi-Mente Le Sorgive',
+      gender: 'M',
+      weight: 66,
+      birth_year: 2000,
+    }]);
+  }catch(e){
+    console.log(e);
+  }
+});
+
+match_router.get('/:matchid', async (req, res) => {
+  const match_id = req.params.matchid;
+  match.findById(match_id);
 });
