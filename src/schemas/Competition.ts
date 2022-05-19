82e458c1b7d103c3d5cb414be4ca8ff13fa43ba1
Marrocco-Simone <marrocco.simone00@gmail.com>
2022-05-19 16:17:43 +0200

    fixed inconsistency between user and competition

--- src/schemas/Competition.ts
+++ src/schemas/Competition.ts
@@ -2,14 +2,10 @@ import { model, Schema } from 'mongoose';
 
 export interface CompetitionInterface {
   name: string;
-  username: string;
-  password: string;
 }
 
 const competition_schema = new Schema<CompetitionInterface>({
   name: String,
-  username: String,
-  password: String,
 });
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
