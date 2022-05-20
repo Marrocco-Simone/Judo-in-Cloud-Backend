3d13a572bd93bac7e6ef8537d4ef225dc89b05be
Ale Rayz <rayz1065@gmail.com>
2022-05-20 19:13:37 +0200

    Changed endpoint for login, added user to login

--- src/routers/auth_router.ts
+++ src/routers/auth_router.ts
@@ -3,4 +3,4 @@ import { login } from '../controllers/auth_controller';
 /** apis for authentication */
 export const auth_router = express.Router();
 
-auth_router.post('/login', login);
+auth_router.post('/', login);
