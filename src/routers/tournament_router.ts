dce382ec0f394a88f582c2e8b96e84388c46c377
simonevazzanaunitn <simone.vazzana@studenti.unitn.it>
2022-05-21 07:49:35 +0200

    feat(tournament): added get route to get all next matches on a specific tournament

--- src/routers/tournament_router.ts
+++ src/routers/tournament_router.ts
@@ -1,5 +1,6 @@
 import express = require('express');
 import { error, success } from '../controllers/base_controller';
+import { Match } from '../schemas';
 import { Tournament } from '../schemas/Tournament';
 /** api for tournaments */
 export const tournament_router = express.Router();
@@ -18,7 +19,14 @@ tournament_router.get('/', async (req, res) => {
 tournament_router.get('/:tournament_id', async (req, res) => {
   try {
     const tournament_id = req.params.tournament_id;
-    const tournament = await Tournament.findById(tournament_id);
+    const tournament = await Tournament.findById(tournament_id)
+      .populate('category')
+      .populate('competition')
+      .populate('athletes')
+      .populate({
+        path: 'winners_bracket',
+        model: 'Match'
+      });
     if (!tournament) throw new Error('No tournament found');
 
     success(res, tournament);
@@ -26,3 +34,52 @@ tournament_router.get('/:tournament_id', async (req, res) => {
     error(res, err.message, 500);
   }
 });
+
+// Get next match for tournament by id
+tournament_router.get('/:tournament_id/next', async (req, res) => {
+  try {
+    const tournament_id = req.params.tournament_id;
+    const tournament = await Tournament.findById(tournament_id)
+      .populate('category')
+      .populate('competition')
+      .populate('athletes')
+      .populate({
+        path: 'winners_bracket',
+        model: 'Match'
+      });
+    if (!tournament) throw new Error('No tournament found');
+
+    const { winners_bracket, finished, recovered_bracket_1, recovered_bracket_2 } = tournament;
+
+    if (finished) success(res, 'The tournament\'s finished, there are no matches left to compete');
+
+    const winners_bracket_next = await getNextMatches({ bracket: winners_bracket });
+    const recovered_bracket_next_1 = await getNextMatches({ bracket: recovered_bracket_1 });
+    const recovered_bracket_next_2 = await getNextMatches({ bracket: recovered_bracket_2 });
+
+    success(res, {
+      winners_bracket_next,
+      recovered_bracket_next_1,
+      recovered_bracket_next_2
+    });
+  } catch (err) {
+    error(res, err.message, 500);
+  }
+});
+
+async function getNextMatches ({ bracket }) {
+  const bracket_next = [];
+
+  for (const round of bracket) {
+    if (!round) continue;
+    for (const match_id of round) {
+      if (!match_id) continue;
+      const match = await Match.findById(match_id);
+      if (!match) throw new Error('Found match_id which doesn\'t reference an existing match');
+
+      if (!match.is_started) bracket_next.push(match);
+    }
+  }
+
+  return bracket_next;
+}
