0401ccb1a6454dc6aef529458c34d6cccff6841b
simonevazzanaunitn <simone.vazzana@studenti.unitn.it>
2022-05-28 12:15:58 +0200

    feat(main): added option to run the app on the test environment

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ import { UserInterface } from './schemas/User';
 const app = express();
 const server_port = process.env.SERVER_PORT;
 const server_url = process.env.SERVER_URL;
-const mongo_url = process.env.MONGO_URL;
+const mongo_url = process.env.ENV === 'test' ? process.env.MONGO_URL_TEST : process.env.MONGO_URL;
 const access_token_secret = process.env.ACCESS_TOKEN_SECRET;
 
 if ([server_port, server_url, mongo_url, access_token_secret].includes(undefined)) {
@@ -23,7 +23,7 @@ if ([server_port, server_url, mongo_url, access_token_secret].includes(undefined
     'and update it with your configuration, then restart.');
 }
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(mongo_url);
 
 // handle type from middlewares
 declare global {
