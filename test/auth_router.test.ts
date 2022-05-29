2f654b95d452150bf3c62e352a79b6098684c9ba
simonevazzanaunitn <simone.vazzana@studenti.unitn.it>
2022-05-29 17:40:49 +0200

    feat(test): added tests on the login post authorization route

--- test/auth_router.test.ts
+++ test/auth_router.test.ts
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import { User } from '../src/schemas';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const { app } = require('../src/bootstrap');
 const node_fetch = require('node-fetch');
@@ -15,10 +16,12 @@ beforeAll(async () => {
 
   await User.remove({});
 
+  const hash = await bcrypt.hash('pwd', 10);
+
   const user_to_save = new User({
     _id: user_id,
     username: 'validUser',
-    password: 'pwd'
+    password: hash
   });
 
   await user_to_save.save();
@@ -77,13 +80,79 @@ test(`GET ${auth_route} should give back the user associated to the jwt if valid
 
   const json_res = await res.json();
 
-  expect(json_res).toEqual({
+  expect(json_res).toMatchObject({
     status: 'success',
     data: {
+      __v: 0,
       _id: user_id.toString(),
-      username: 'validUser',
-      password: 'pwd',
-      __v: 0
+      username: 'validUser'
     }
+  });
+});
+
+test(`POST ${auth_route} should give an error message if there is no username or password inside the body`, async () => {
+  const res = await node_fetch(auth_route, {
+    method: 'POST'
+  });
+
+  const json_res = await res.json();
+
+  expect(json_res).toEqual({
+    message: 'Inserisci username e password',
+    status: 'fail'
+  });
+});
+
+test(`POST ${auth_route} should give an error message if the password inside the body is wrong`, async () => {
+  const req_body = {
+    username: 'validUser',
+    password: 'wrongPwd'
+  };
+
+  const res = await node_fetch(auth_route, {
+    method: 'POST',
+    body: JSON.stringify(req_body),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  const json_res = await res.json();
+
+  expect(json_res).toEqual({
+    message: 'Nome utente o password non validi',
+    status: 'fail'
+  });
+});
+
+test(`POST ${auth_route} should correctly log in a user, given valid username and password inside the body`, async () => {
+  const valid_user = { _id: user_id, username: 'validUser' };
+  const access_jwt = jwt.sign(valid_user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 24 });
+
+  const req_body = {
+    username: 'validUser',
+    password: 'pwd'
+  };
+
+  const res = await node_fetch(auth_route, {
+    method: 'POST',
+    body: JSON.stringify(req_body),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  const json_res = await res.json();
+
+  expect(json_res).toMatchObject({
+    data: {
+      access_token: access_jwt,
+      user: {
+        __v: 0,
+        _id: user_id,
+        username: 'validUser'
+      }
+    },
+    status: 'success'
   });
 });
