6c07cad6740100ff07e910153d38b31218ecb8b1
Marrocco-Simone <marrocco.simone00@gmail.com>
2022-05-31 15:18:07 +0200

    merge public competitions

--- src/schemas/Competition.ts
+++ src/schemas/Competition.ts
@@ -3,10 +3,12 @@ import { model, Schema, Types } from 'mongoose';
 export interface CompetitionInterface {
   _id?: Types.ObjectId;
   name: string;
+  slug: string;
 }
 
 const competition_schema = new Schema<CompetitionInterface>({
   name: String,
+  slug: String,
 });
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
