b1e3c6693247a0f91b56ab9e55cc0643e3144528
rayz1065 <rayz1065@gmail.com>
2022-06-02 19:18:08 +0200

    Refactored matches api to use correct api version

--- src/routers/api_v1.ts
+++ src/routers/api_v1.ts
@@ -1,6 +1,7 @@
 import app from 'express';
 import { get_age_class, get_age_classes, update_age_class } from '../controllers/age_classes_controller';
 import { create_athlete, get_athletes } from '../controllers/athletes_controller';
+import { get_match, update_match } from '../controllers/matches_controller';
 import { authenticate_token } from '../middlewares/AuthenticateMiddleware';
 import { requires_competition } from '../middlewares/RequiresCompetition';
 
@@ -19,5 +20,12 @@ athletes_router.use(authenticate_token, requires_competition);
 athletes_router.get('/', get_athletes);
 athletes_router.post('/', create_athlete);
 
+// routes for matches
+const matches_router = app.Router();
+matches_router.use(authenticate_token, requires_competition);
+matches_router.get('/:match_id', get_match);
+matches_router.post('/:match_id', update_match);
+
 api_v1_router.use('/age_classes', age_classes_router);
 api_v1_router.use('/athletes', athletes_router);
+api_v1_router.use('/matches', matches_router);
