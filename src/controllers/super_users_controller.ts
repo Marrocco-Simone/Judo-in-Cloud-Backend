0b690a679dbb31d7585c2497c31f08a32e63c480
Marrocco-Simone <marrocco.simone00@gmail.com>
2022-06-03 18:28:10 +0200

    files for superuser api

---
+++ src/controllers/super_users_controller.ts
@@ -0,0 +1,5 @@
+import express from 'express';
+import { RequestHandler } from 'express';
+import { success, error, fail } from './base_controller';
+
+export const test: RequestHandler = async (req, res) => {}
