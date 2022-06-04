bf0ab28923e72db78176421559d0cd92c32e05d1
Ale Rayz <rayz1065@gmail.com>
2022-06-04 18:40:23 +0200

    Added test that tries to delete a non-existent athlete

--- test/athlete_router.test.ts
+++ test/athlete_router.test.ts
@@ -511,3 +511,21 @@ test(`DELETE ${athlete_1_route} should succeed`, async () => {
   const athlete = await Athlete.findById(athlete_id_1);
   expect(athlete).toBeNull();
 });
+
+test(`DELETE ${nonexistent_athlete_route} should fail with status 404 not found`, async () => {
+  const valid_user = { _id: user_id_1, username: 'validUser' };
+  const access_jwt = jwt.sign(valid_user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 24 });
+  const authorization = `Bearer ${access_jwt}`;
+
+  const res = await node_fetch(nonexistent_athlete_route, {
+    method: 'DELETE',
+    headers: { authorization }
+  });
+
+  expect(res.status).toBe(404);
+
+  const json_res = await res.json();
+
+  expect(json_res.status).toBe('fail');
+  expect(json_res).toHaveProperty('message');
+});
