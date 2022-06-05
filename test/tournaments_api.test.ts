82cd2cbaa360778372a9226cbac337d53dc8fcf5
Ale Rayz <rayz1065@gmail.com>
2022-06-05 14:25:39 +0200

    Added test that tries to reserve a tournament without proper authorization

--- test/tournaments_api.test.ts
+++ test/tournaments_api.test.ts
@@ -12,10 +12,12 @@ let server: Server;
 const user_id_1 = new mongoose.Types.ObjectId();
 const competition_id = new mongoose.Types.ObjectId();
 const tournament_id = new mongoose.Types.ObjectId();
+const unauth_tournament_id = new mongoose.Types.ObjectId();
 
 const tour_reserve_route = `http://localhost:2500/api/v2/tournaments/reserve/${tournament_id}`;
 const non_existent_tour_reserve_route = `http://localhost:2500/api/v2/tournaments/reserve/${new mongoose.Types.ObjectId()}`;
 const invalid_tour_reserve_route = 'http://localhost:2500/api/v2/tournaments/reserve/123';
+const unauth_tour_reserve_route = `http://localhost:2500/api/v2/tournaments/reserve/${unauth_tournament_id}`;
 
 beforeAll(async () => {
   mongoose.connect(process.env.MONGO_URL_TEST);
@@ -51,8 +53,20 @@ beforeEach(async () => {
     recovered_bracket_1: [],
     recovered_bracket_2: [],
   });
+  const unauth_tournament = new Tournament({
+    _id: unauth_tournament_id,
+    competition: competition_id,
+    category: new mongoose.Types.ObjectId(),
+    tatami_number: null,
+    finished: false,
+    athletes: [],
+    winners_bracket: [],
+    recovered_bracket_1: [],
+    recovered_bracket_2: [],
+  });
   await Tournament.deleteMany({});
   await tournament.save();
+  await unauth_tournament.save();
 });
 
 afterAll(async () => {
@@ -62,8 +76,8 @@ afterAll(async () => {
 });
 
 test(`POST ${tour_reserve_route} should reserve the tournament`, async () => {
-  const invalid_user = { _id: user_id_1, username: 'validUser' };
-  const access_jwt = jwt.sign(invalid_user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 24 });
+  const valid_user = { _id: user_id_1, username: 'validUser' };
+  const access_jwt = jwt.sign(valid_user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 24 });
   const authorization = `Bearer ${access_jwt}`;
 
   const res = await node_fetch(tour_reserve_route, {
@@ -79,8 +93,8 @@ test(`POST ${tour_reserve_route} should reserve the tournament`, async () => {
 });
 
 test(`POST ${non_existent_tour_reserve_route} should fail with status 404 not found`, async () => {
-  const invalid_user = { _id: user_id_1, username: 'validUser' };
-  const access_jwt = jwt.sign(invalid_user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 24 });
+  const valid_user = { _id: user_id_1, username: 'validUser' };
+  const access_jwt = jwt.sign(valid_user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 24 });
   const authorization = `Bearer ${access_jwt}`;
 
   const res = await node_fetch(non_existent_tour_reserve_route, {
@@ -96,8 +110,8 @@ test(`POST ${non_existent_tour_reserve_route} should fail with status 404 not fo
 });
 
 test(`POST ${invalid_tour_reserve_route} should fail with status 400 since the id is invalid`, async () => {
-  const invalid_user = { _id: user_id_1, username: 'validUser' };
-  const access_jwt = jwt.sign(invalid_user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 24 });
+  const valid_user = { _id: user_id_1, username: 'validUser' };
+  const access_jwt = jwt.sign(valid_user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 24 });
   const authorization = `Bearer ${access_jwt}`;
 
   const res = await node_fetch(invalid_tour_reserve_route, {
@@ -113,8 +127,8 @@ test(`POST ${invalid_tour_reserve_route} should fail with status 400 since the i
 });
 
 test(`POST ${tour_reserve_route} without passing a valid tatami number should fail with status 400`, async () => {
-  const invalid_user = { _id: user_id_1, username: 'validUser' };
-  const access_jwt = jwt.sign(invalid_user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 24 });
+  const valid_user = { _id: user_id_1, username: 'validUser' };
+  const access_jwt = jwt.sign(valid_user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 24 });
   const authorization = `Bearer ${access_jwt}`;
 
   const invalid_bodies = [
@@ -136,4 +150,21 @@ test(`POST ${tour_reserve_route} without passing a valid tatami number should fa
     expect(json_res.status).toBe('fail');
     expect(json_res).toHaveProperty('message');
   }
+});
+
+test(`POST ${unauth_tour_reserve_route} should not allow changes and fail with status 403 unauthorized`, async () => {
+  const valid_user = { _id: user_id_1, username: 'validUser' };
+  const access_jwt = jwt.sign(valid_user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 24 });
+  const authorization = `Bearer ${access_jwt}`;
+
+  const res = await node_fetch(unauth_tour_reserve_route, {
+    method: 'POST',
+    body: JSON.stringify({ tatami_number: 1 }),
+    headers: { authorization, 'Content-Type': 'application/json' },
+  });
+  expect(res.status).toBe(403);
+
+  const json_res = await res.json();
+  expect(json_res.status).toBe('fail');
+  expect(json_res).toHaveProperty('message');
 });
