f54b6d73656574416265e00f8ea6a80482c84f49
Ale Rayz <rayz1065@gmail.com>
2022-06-05 13:58:55 +0200

    Added test that tries to reserve a tournament with an invalid id

--- test/tournaments_api.test.ts
+++ test/tournaments_api.test.ts
@@ -92,7 +92,22 @@ test(`POST ${non_existent_tour_reserve_route} should fail with status 404 not fo
 
   const json_res = await res.json();
   expect(json_res.status).toBe('fail');
+  expect(json_res).toHaveProperty('message');
 });
 
 test(`POST ${invalid_tour_reserve_route} should fail with status 400 since the id is invalid`, async () => {
+  const invalid_user = { _id: user_id_1, username: 'validUser' };
+  const access_jwt = jwt.sign(invalid_user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 24 });
+  const authorization = `Bearer ${access_jwt}`;
+
+  const res = await node_fetch(invalid_tour_reserve_route, {
+    method: 'POST',
+    body: JSON.stringify({ tatami_number: 1 }),
+    headers: { authorization, 'Content-Type': 'application/json' },
+  });
+  expect(res.status).toBe(400);
+
+  const json_res = await res.json();
+  expect(json_res.status).toBe('fail');
+  expect(json_res).toHaveProperty('message');
 });
