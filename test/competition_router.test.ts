c472fc729121e934774a69a5bd8710c50458b155
Leonardo Porcelli <leonardo.porcelli@studenti.unitn.it>
2022-06-06 22:41:57 +0200

    Testing of competition

--- test/competition_router.test.ts
+++ test/competition_router.test.ts
@@ -6,7 +6,6 @@ import bcrypt from 'bcrypt';
 import { app } from '../src/bootstrap';
 import node_fetch from 'node-fetch';
 import { Server } from 'http';
-import { test } from '../src/controllers/super_users_controller';
 
 let server: Server;
 
@@ -15,7 +14,7 @@ const user_id_1 = new mongoose.Types.ObjectId();
 const competition_slug = 'gara-lavis';
 
 const competition_slug_route = `http://localhost:2500/api/v2/competitions/find/${competition_slug}`;
-const invalid_slug_route = `http://localhost:2500/api/v2/competitions/find/invalid-slug`;
+const invalid_slug_route = 'http://localhost:2500/api/v2/competitions/find/invalid-slug';
 
 beforeAll(async () => {
   mongoose.connect(process.env.MONGO_URL_TEST);
@@ -45,4 +44,29 @@ afterAll(async () => {
   server.close();
 });
 
-test(`GET ${competition_slug_route} `)
+test(`GET ${competition_slug_route} should return the competition`, async () => {
+  const valid_user = { _id: user_id_1, username: 'validUser' };
+  const access_jwt = jwt.sign(valid_user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 24 });
+  const authorization = `Bearer ${access_jwt}`;
+  const res = await node_fetch(competition_slug_route, {
+    method: 'GET',
+    headers: { authorization, 'Content-Type': 'application/json' },
+  });
+  expect(res.status).toBe(200);
+  const json_res = await res.json();
+  expect(json_res.status).toBe('success');
+});
+
+test(`GET ${invalid_slug_route} should fail with status 404 not found`, async () => {
+  const valid_user = { _id: user_id_1, username: 'validUser' };
+  const access_jwt = jwt.sign(valid_user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 24 });
+  const authorization = `Bearer ${access_jwt}`;
+  const res = await node_fetch(invalid_slug_route, {
+    method: 'GET',
+    headers: { authorization, 'Content-Type': 'application/json' },
+  });
+  expect(res.status).toBe(404);
+  const json_res = await res.json();
+  expect(json_res.status).toBe('fail');
+  expect(json_res).toHaveProperty('message');
+});
