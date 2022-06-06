6bf8471ff6a5d1b25a236c98e64bd1b0f7fa4afb
Ale Rayz <rayz1065@gmail.com>
2022-06-06 21:59:40 +0200

    Created test that tries to generate a leaderboard for an unfinished tournament

--- test/tournaments_api.test.ts
+++ test/tournaments_api.test.ts
@@ -243,3 +243,18 @@ test(`GET ${leaderboard_route} should return the final leaderboard`, async () =>
     { place: 5, athlete: player_fifth_2.toString() },
   ]);
 });
+
+test(`GET ${unfinished_leaderboard_route} should fail with status 400 since the tournament is not finished`, async () => {
+  const valid_user = { _id: user_id_1, username: 'validUser' };
+  const access_jwt = jwt.sign(valid_user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 24 });
+  const authorization = `Bearer ${access_jwt}`;
+
+  const res = await node_fetch(unfinished_leaderboard_route, {
+    method: 'GET',
+    headers: { authorization },
+  });
+  const json_res = await res.json();
+
+  expect(res.status).toBe(400);
+  expect(json_res.status).toBe('fail');
+});
